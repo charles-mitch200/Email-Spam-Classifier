fdfcb13fd3d973ced01efc9c2aa25a6846fe0f06
charles-mitch200 <charlesreign2@gmail.com>
2023-08-23 11:07:48 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+
+tokenizer = pickle.load(open("models/cv.pkl", "rb"))
+model = pickle.load(open("models/clf.pkl", "rb"))
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email = request.form.get("email-content")
+
+    # Tokenize text
+    tokenized_email = tokenizer.transform([email])
+    prediction = model.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+
+    return render_template("index.html", **locals())
+
+if __name__=="__main__":
+    app.run(debug=True)
